9aaa0ae349a3b7d03739c2fefb34dd7f50b77ff0
Orlando <santiago@etown.edu>
2025-08-05 11:04:54 -0400

    Added movement to the player and plan to add an enemy to start the battle

--- CombatSystem/j.js
+++ CombatSystem/j.js
@@ -6,19 +6,25 @@ const graphics = canvas.getContext('2d')
 
 let playerHealth = 100
 let monsterHealth = 100
-let fps = 60
+let fps = 3
 
-
-let x = 20
-let y =100
-let speed = 1
+movingUp = false
+movingDown = false
+movingLeft = false
+movingRight = false
 
 
 
 let playerX = canvas.width / 2 // Initial X position
 let playerY = canvas.height / 2 // Initial Y position
-const playerRadius = 20
-const moveSpeed = 5
+const playerRadius = 10
+const moveSpeed = 1
+
+
+const choice1 = document.getElementById("Option1")
+const choice2 = document.getElementById("Option2")
+const choice3 = document.getElementById("Option3")
+const choice4 = document.getElementById("Option4")
 
 
 function clear(){
@@ -28,11 +34,18 @@ function clear(){
 }
 
 
-function battle(){
+function animate(){
     clear()
     player()
-    
+    movement()
 }
+// The choices the player will have during the fight
+function Fight(){
+
+}
+
+
+
 
 //randomly, while the player is romping around, a battle will start
 function startBattle(){
@@ -42,7 +55,12 @@ function startBattle(){
     battleLoop()
 }
 
-
+//if fuction where if the player is in a certain area, a battle will start
+function checkForBattle(){
+    if(Math.random > 0.1){
+        startBattle()
+    }
+}
 
 function monsterAttack(){
     let damage = Math.floor(Math.random() * 20) + 1 // random damage between 1 and 20
@@ -54,14 +72,14 @@ function battleLoop(){
     while(playerHealth > 0 && monsterHealth > 0){
         let playerAction = prompt("Choose your action: (1) Attack (2) Heal")
         if(playerAction === "1"){
-            let damage = Math.floor(Math.random() * 20) + 1 // random damage between 1 and 20
+            let damage = Math.floor(Math.random() * 20) + 1 // randomdamage between 1 and 20
             monsterHealth -= damage
             console.log(`You attack! Monster takes ${damage} damage. Monster health: ${monsterHealth}`)
         } else if(playerAction === "2"){
-            let heal = Math.floor(Math.random() * 15) + 1 // random heal between 1 and 15
+            let heal = Math.floor(Math.random() * 15) + 1 // randomly heals between 1 and 15
             playerHealth += heal
             console.log(`You heal for ${heal}. Player health: ${playerHealth}`)
-        } else {
+        } else{
             console.log("Invalid action. Try again.")
             continue
         }
@@ -82,34 +100,69 @@ function battleLoop(){
 
 
 // Creates the player
-function player(){
-
+function player(){ 
     graphics.fillStyle= "yellow"
     graphics.beginPath()
-    graphics.arc(x,y,playerRadius,0,Math.PI*2)
+    graphics.arc(playerX, playerY, playerRadius, 0, Math.PI*2)
     graphics.fill()
     graphics.closePath()
 }
+// Creates the enemy
 
 // Movement for the player
-document.addEventListener("keydown", (event) => {
-    switch (event.key) {
-        case "ArrowUp":
-            playerY -= moveSpeed
-            break
-        case "ArrowDown":
-            playerY += moveSpeed
-            break
-        case "ArrowLeft":
-            playerX -= moveSpeed
-            break
-        case "ArrowRight":
-            playerX += moveSpeed
-            break
-        }
-        player()
+document.addEventListener("keydown", (event)=>{
+    if(event.key === "ArrowUp"){
+        movingUp = true
+    }else if(event.key === "ArrowDown"){
+        movingDown = true
     }
-)
+})
+
+document.addEventListener("keyup",(event)=>{
+    if(event.key === "ArrowUp"){
+        movingUp = false
+    }else if(event.key === "ArrowDown"){
+        movingDown = false
+    }
+})
+
+document.addEventListener("keydown",(event)=>{
+    if(event.key === "ArrowLeft"){
+        movingLeft = true
+    }else if(event.key === "ArrowRight"){
+        movingRight = true
+    }
+})
+document.addEventListener("keyup",(event)=>{
+    if(event.key === "ArrowLeft"){
+        movingLeft = false
+    }else if(event.key === "ArrowRight"){
+        movingRight = false
+    }
+})
+
+function movement(){
+    if(movingUp){
+        playerY -= moveSpeed * fps
+    }
+
+    if(movingDown){
+        playerY +=moveSpeed * fps
+    }
+
+    if(movingLeft){
+        playerX -= moveSpeed * fps
+    }
+
+    if(movingRight){
+        playerX += moveSpeed * fps
+    }
+}
+
+
+
+
+
 
 
 window.setInterval(animate,fps/1000)

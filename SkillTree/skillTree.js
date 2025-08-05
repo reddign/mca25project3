fc09cc1ce3f828f89e813c8712a49aacd77fa934
Daniel <spooner@etown.edu>
2025-08-05 13:08:42 -0400

    Incomplete new design, taking break

--- SkillTree/skillTree.js
+++ SkillTree/skillTree.js
@@ -2,24 +2,44 @@ let canvas = document.querySelector("canvas");
 //Gets Graphics
 const graphics = canvas.getContext("2d");
 
+let stat = [0,0,0,0,0];
+let statNames = ["Health","DMG","Mana","P","P"];
+let imgPos = [];
 let upgrades = [];
 let upgradeOwned = [];
 let prerequisites = [];
-let Health = 0;
+let points=1;
 
 createUpgradeBasic("Health",1,[])
-createUpgradeBasic("Health",3,[1])
+createUpgradeBasic("Health",3,[0])
 createUpgradeBoxes();
+//newDesign();
+//console.log(imgPos)
 
 function createUpgradeBasic(stat,amount,prerequisite){
     upgrades.push([stat,amount]);
-    prerequisites.push([prerequisite]);
-    upgradeOwned.push([false]);
+    prerequisites.push(prerequisite);
+    upgradeOwned.push(false);
+}
+
+function newDesign(){
+    for(let i=0; i<upgrades.length; i++){
+        let paramHeight = 0;
+        for(let j=0; j<prerequisites[i].length; j++){
+            if(paramHeight<=imgPos){
+                paramHeight = imgPos[prerequisites[j]]+1;
+            }
+        }
+        if(paramHeight>=imgPos.length){
+            imgPos.push([]);
+        }
+        imgPos[paramHeight].push(i);
+    }
 }
 
 function createUpgradeBoxes(){
     let row=0;
-    let rowLength=0;
+    let rowLength=16;
     for(let i=0; i<upgrades.length; i++){
         if(row==0&&i*20>canvas.width){
             row++;
@@ -36,17 +56,29 @@ function createUpgradeBoxes(){
     }
 }
 
-function getUpgrade(){
-
+function getUpgrade(x,y){
+    let xSlot = Math.floor(x/20);
+    let ySlot = Math.floor(y/20);
+    if(points>0){
+        let upgradeSelect = xSlot+ySlot*16;
+        let canGet = true;
+        for(let i=0; i<prerequisites[upgradeSelect].length; i++){
+            if(!upgradeOwned[prerequisites[upgradeSelect][i]]){
+                canGet = false;
+            }
+        }
+        if(canGet&&!upgradeOwned[upgradeSelect]){
+            upgradeOwned[upgradeSelect]=true;
+            //stat[statNames.findIndex(upgrades[upgradeSelect][0])]+=upgrades[upgradeSelect][1];
+        }else{
+            console.log("failed")
+        }
+    }
 }
 
 function press(event){
     let canvasrect = canvas.getBoundingClientRect();
     let x = (event.clientX-canvasrect.x)/3.35;
     let y = (event.clientY-canvasrect.y)/3.35;
-    if(x>canvas.width-60){
-        buttonPress(x,y);
-    }else if(x<60&&y<20){
-        backPage();
-    }
+    getUpgrade(x,y)
 }

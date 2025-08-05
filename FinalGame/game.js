51b5e38250e077304c097880bb7aabfe969a1589
Daniel <spooner@etown.edu>
2025-08-05 13:54:27 -0400

    Added Upgrades

--- FinalGame/game.js
+++ FinalGame/game.js
@@ -3,19 +3,14 @@ const graphics = canvas.getContext("2d");
 
 let screen = "Home";
 
+//General
 function animate(){
     if(screen=="Home"){
-        createHome();
+        createHomePage();
     }
-}
-
-function createHome(){
-    graphics.fillStyle = "White";
-    graphics.fillRect(canvas.width/2-30,canvas.height/2+30,60,15);
-    graphics.fillRect(canvas.width/2-30,canvas.height/2+50,60,15);
-    graphics.fillStyle = "Black";
-    graphics.fillText("Play",canvas.width/2-10,canvas.height/2+41);
-    graphics.fillText("Upgrades",canvas.width/2-21,canvas.height/2+61)
+    if(screen=="Upgrade"){
+        createUpgradePage();
+    }
 }
 
 function press(event){
@@ -27,15 +22,91 @@ function press(event){
             if(y>canvas.height/2+30&&y<canvas.height/2+45){
                 screen="Game";
                 graphics.clearRect(0,0,canvas.width,canvas.height);
-                console.log("Game")
             }
             if(y>canvas.height/2+50&&y<canvas.height/2+65){
                 screen="Upgrade";
                 graphics.clearRect(0,0,canvas.width,canvas.height);
-                console.log("Upgrade")
             }
         }
+    }else if(screen=="Upgrade"){
+        if(x>canvas.width-60){
+            buttonPress(x,y);
+        }else if(x<60&&y<20){
+            backPage();
+        }
     }
+}
+
+//Home
+function createHomePage(){
+    graphics.fillStyle = "White";
+    graphics.fillRect(canvas.width/2-30,canvas.height/2+30,60,15);
+    graphics.fillRect(canvas.width/2-30,canvas.height/2+50,60,15);
+    graphics.fillStyle = "Black";
+    graphics.fillText("Play",canvas.width/2-10,canvas.height/2+41);
+    graphics.fillText("Upgrades",canvas.width/2-21,canvas.height/2+61)
+}
+
+//Upgrades
+let stat = [0,0,0,0,0];
+let statNames = ["Health","DMG","Mana","P","P"];
+let upgradeCost = [1,1,1,1,1];
+let upgradeAmount = [1,1,1,1,1];
+let coins = 50;
+let buttons=[];
+
+function createUpgradePage(){
+    createAllUpgradeButtons();
+    createButton(0,0,60,20,"Back");
+    showCoins();
+}
+
+function backPage(){
+    screen="Home";
+    graphics.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function createAllUpgradeButtons(){
+    for(let i=0; i<statNames.length; i++){
+        createUpgradeButton(canvas.width-60,i*20,60,20,i,statNames[i],upgradeAmount[i],upgradeCost[i]);
+    }
+}
+
+function createButton(x,y,width,height,text){
+    graphics.strokeStyle = "#2c3e50";
+    graphics.fillStyle = "#f7f7f7";
+    graphics.strokeRect(x,y,width,height)
+    graphics.fillRect(x,y,width,height);
+    graphics.fillStyle = "#a50034";
+    graphics.fillText(text,x+10,y+13)
+}
+
+function createUpgradeButton(x,y,width,height,statNum,statName,amount,cost){
+    graphics.strokeStyle = "#2c3e50";
+    graphics.fillStyle = "#f7f7f7";
+    graphics.strokeRect(x,y,width,height)
+    graphics.fillRect(x,y,width,height);
+    graphics.fillStyle = "#a50034";
+    graphics.fillText(statName+": "+Math.floor(cost),x+10,y+13)
+    buttons.push([statNum,amount]);
+}
+
+function buttonPress(x,y){
+    if(x>canvas.width-60){
+        if(coins>=Math.floor(upgradeCost[Math.floor(y/20)])){
+            stat[buttons[Math.floor(y/20)][0]] += buttons[Math.floor(y/20)][1];
+            coins-=Math.floor(upgradeCost[Math.floor(y/20)]);
+            upgradeCost[Math.floor(y/20)] *= 1.1;
+            graphics.clearRect(x,y,canvas.width,canvas.height);
+            createAllUpgradeButtons();
+        }else{
+            alert("Too expensive");
+        }
+    }
+}
+
+function showCoins(){
+    createButton(60,0,60,20,"Coins: " + coins)
 }
 
 let game = setInterval(animate,1000/60);
